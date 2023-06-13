eef048310ee7100abc85516a97538f3f325585a4
611727341 <611727341@BT-PF400F5T>
2023-06-13 14:21:35 +0100

    Add project files.

---
+++ JasonWTest_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JasonWTest_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("JasonWTest_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F2AD11EF-1961-4749-B02B-43D20D3D76E0")]
+[assembly: AssemblyVersion("1.0.0.0")]
